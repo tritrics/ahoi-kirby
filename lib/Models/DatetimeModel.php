f4b1572c65a74ff8cb4d73d8801b0c080f4e22b2
tritrics <ma@tritrics.dk>
2024-03-10 17:11:55 +0100

    update DateModel

--- lib/Models/DatetimeModel.php
+++ lib/Models/DatetimeModel.php
@@ -25,9 +25,13 @@ class DatetimeModel extends BaseModel
       $utc = [(int) date('Y', $time), (int) date('m', $time) - 1, (int) date('d', $time),(int) date('H', $time), (int) date('i', $time), (int) date('s', $time), 0];
     }
     $meta = new Collection();
+
+    $timezone = date_default_timezone_get();
+    date_default_timezone_set('UTC');
+    $meta->add('utc', date('Y-m-d\TH:i:s\Z', $time));
+    date_default_timezone_set($timezone);
     $meta->add('iso', date('c', $time));
-    // $meta->add('jsdate', implode(',', $utc)); JS: new Date(Date.UTC(...obj.meta.jsdate.split(','))),
-    $meta->add('timezone', date_default_timezone_get());
+    $meta->add('timezone', $timezone);
 
     $res = new Collection();
     $res->add('meta', $meta);
